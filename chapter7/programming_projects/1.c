19a162c239e1d551854a0e2db92490d9b374c8ea
udkfer <udkfer@gmail.com>
2025-05-26 20:28:43 -0300

    Work in Progress Chapter 7

--- chapter7/programming_projects/1.c
+++ chapter7/programming_projects/1.c
@@ -1,5 +1,5 @@
 // 1. The square2.c program of Section 6.3 will fail (usually by printing strange answers) if i * i exceeds the maximum int value. Run the program and determine the smallest value of n that causes failure. Try changing the type of i to short and running the program again. (Don’t forget to update the conversion specifications in the call of printf!) Then try long. From these experiments, what can you conclude about the number of bits used to store integer types on your machine?
-// A:
+// A: Int: 2146192708, Short: 32767, Long: 9223372036854775807
 
 #include <limits.h>
 #include <stdio.h>
@@ -7,17 +7,18 @@
 int main(void)
 {
 
-  int i, n;
-
-  printf("%d\n", INT_MAX);
+  // int n, i;
+  int n;
+  // short i;
+  long i;
 
   printf("This program prints a table of squares.\n");
   printf("Enter number of entries in table: ");
   scanf("%d", &n);
 
 
-  for (i = 1; i <= n; i++)
-    printf("%10d %10d\n", i, i * i);
+  for (i = 1; i <= n && i > 0; i++)
+    printf("%10ld\t%10ld\n", i, i * i);
 
   return 0;
 }
