708fef4aea8d5377db22b4bb8fde09385540c2b1
udkfer <udkfer@gmail.com>
2025-05-29 20:49:33 -0300

    Work in Progress Chapter 7

---
+++ chapter7/programming_projects/2.c
@@ -0,0 +1,23 @@
+// 2. Modify the square2.c program of Section 6.3 so that it pauses after every 24 squares and displays the following message:
+
+// Press Enter to continue...
+
+// After displaying the message, the program should use getchar to read a character .getchar won’t allow the program to continue until the user presses the Enter key.
+
+/* Prints a table of squares using a for statement */
+#include <stdio.h>
+
+int main(void)
+{
+  int i, n;
+
+  printf("This program prints a table of squares.\n");
+  printf("Enter number of entries in table: ");
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; i++)
+    printf("%10d%10d\n", i, i * i);
+
+  return 0;
+}
+
